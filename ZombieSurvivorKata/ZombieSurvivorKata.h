9ffff76fb726c553e9f034b967feece71ae4c3e9
Pablo Manuelli <pablo.manuelli@etermax.com>
2020-05-30 18:56:36 -0300

    Initial Commit

---
+++ ZombieSurvivorKata/ZombieSurvivorKata.h
@@ -0,0 +1,19 @@
+//
+//  ZombieSurvivorKata.h
+//  ZombieSurvivorKata
+//
+//  Created by Pablo Manuelli on 30/05/2020.
+//  Copyright © 2020 pablomanuelli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZombieSurvivorKata.
+FOUNDATION_EXPORT double ZombieSurvivorKataVersionNumber;
+
+//! Project version string for ZombieSurvivorKata.
+FOUNDATION_EXPORT const unsigned char ZombieSurvivorKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZombieSurvivorKata/PublicHeader.h>
+
+
