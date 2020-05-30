41431cb25b1df724a16ff520dc043ca3fdf33d0a
Pablo Manuelli <pablo.manuelli@etermax.com>
2020-05-30 19:37:47 -0300

    Survivors have no initial wounds

--- ZombieSurvivorKataTests/SurvivorShould.swift
+++ ZombieSurvivorKataTests/SurvivorShould.swift
@@ -4,15 +4,25 @@ import XCTest
 
 class SurvivorShould: XCTestCase {
 
+    private let survivorName = "Chuker"
+    
     func test_haveAName() {
         
-        let survivor = Survivor(name: "Chuker")
+        let survivor = Survivor(name: survivorName)
         
-        XCTAssertEqual("Chuker", survivor.name)
+        XCTAssertEqual(survivorName, survivor.name)
+    }
+    
+    func test_haveNoInitialWounds() {
+        
+        let survivor = Survivor(name: survivorName)
+        
+        XCTAssertEqual(0, survivor.wounds)
     }
 }
 
 struct Survivor {
     
     let name: String
+    let wounds = 0
 }
