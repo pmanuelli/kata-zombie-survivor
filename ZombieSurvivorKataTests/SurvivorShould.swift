456fa848169f539ffe4e2936978741e74cacab5e
Pablo Manuelli <pablo.manuelli@etermax.com>
2020-05-30 20:01:07 -0300

    Survivors can receive wounds

--- ZombieSurvivorKataTests/SurvivorShould.swift
+++ ZombieSurvivorKataTests/SurvivorShould.swift
@@ -21,11 +21,31 @@ class SurvivorShould: XCTestCase {
         
         XCTAssertTrue(survivor.isAlive)
     }
+    
+    func test_receiveWounds() {
+        
+        survivor = survivor.wound()
+        
+        XCTAssertEqual(1, survivor.wounds)
+    }    
 }
 
 struct Survivor {
     
     let name: String
-    let wounds = 0
+    let wounds: Int
     let isAlive = true
+    
+    init(name: String) {
+        self.init(name: name, wounds: 0)
+    }
+    
+    private init(name: String, wounds: Int) {
+        self.name = name
+        self.wounds = wounds
+    }
+    
+    func wound() -> Survivor {
+        return Survivor(name: name, wounds: wounds + 1)
+    }
 }
