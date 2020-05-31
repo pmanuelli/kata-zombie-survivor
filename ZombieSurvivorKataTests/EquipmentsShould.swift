37a57ed7b4cfd0c66b277c5716bfea6c4b6c9296
Pablo Manuelli <pablo.manuelli@etermax.com>
2020-05-31 12:49:37 -0300

    Equipments can add new equipment

--- ZombieSurvivorKataTests/EquipmentsShould.swift
+++ ZombieSurvivorKataTests/EquipmentsShould.swift
@@ -4,17 +4,40 @@ import XCTest
 
 class EquipmentsShould: XCTestCase {
 
-    func test_beCreatedEmpty() {
+    func test_beInitiallyEmpty() {
         
         let equipments = Equipments()
      
         XCTAssertEqual(0, equipments.usedCapacity)
     }
+    
+    func test_addEquipment() {
+        
+        var equipments = Equipments()
+        let equipment = Equipment(name: "Baseball Bat")
+        
+        equipments = equipments.add(equipment)
+        
+        XCTAssertEqual(1, equipments.usedCapacity)
+    }
 }
 
 struct Equipments {
     
-    let usedCapacity = 0
+    var usedCapacity: Int { equipments.count }
+    let equipments: [Equipment]
+    
+    init() {
+        self.init(equipments: [])
+    }
+    
+    private init(equipments: [Equipment]) {
+        self.equipments = equipments
+    }
+    
+    func add(_ equipment: Equipment) -> Equipments {
+        return Equipments(equipments: equipments + [equipment])
+    }
 }
 
 struct Equipment: Equatable {
