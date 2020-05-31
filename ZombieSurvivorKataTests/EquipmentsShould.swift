5e185a553782b4123dc072a65366c3d001efebed
Pablo Manuelli <pablo.manuelli@etermax.com>
2020-05-31 13:01:36 -0300

    Equipments have maximum capacity

--- ZombieSurvivorKataTests/EquipmentsShould.swift
+++ ZombieSurvivorKataTests/EquipmentsShould.swift
@@ -20,22 +20,47 @@ class EquipmentsShould: XCTestCase {
         
         XCTAssertEqual([baseballBat], equipments.equipments)
     }
+    
+    func test_addEquipmentUpToMaximumCapacity() {
+        
+        var equipments = Equipments(maximumCapacity: 2)
+        let baseballBat = Equipment(name: "Baseball Bat")
+        let fryingPan = Equipment(name: "FryingPan")
+        let katana = Equipment(name: "Katana")
+        
+        equipments = equipments.add(baseballBat).add(fryingPan).add(katana)
+        
+        XCTAssertEqual([baseballBat, fryingPan], equipments.equipments)
+    }
 }
 
 struct Equipments {
     
     let equipments: [Equipment]
+    let maximumCapacity: Int
     
     init() {
-        self.init(equipments: [])
+        self.init(maximumCapacity: 5, equipments: [])
     }
     
-    private init(equipments: [Equipment]) {
+    init(maximumCapacity: Int) {
+        self.init(maximumCapacity: maximumCapacity, equipments: [])
+    }
+    
+    private init(maximumCapacity: Int, equipments: [Equipment]) {
+        self.maximumCapacity = maximumCapacity
         self.equipments = equipments
     }
     
     func add(_ equipment: Equipment) -> Equipments {
-        return Equipments(equipments: equipments + [equipment])
+        
+        var equipments = self.equipments
+        
+        if equipments.count < maximumCapacity {
+            equipments = equipments + [equipment]
+        }
+        
+        return Equipments(maximumCapacity: maximumCapacity, equipments: equipments)
     }
 }
 
