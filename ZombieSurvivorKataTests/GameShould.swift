431e0ad1e6d5ddba6d2a90aa2bf5741fefb36c80
Pablo Manuelli <pablo.manuelli@etermax.com>
2020-05-31 19:27:58 -0300

    A Game can add survivors

--- ZombieSurvivorKataTests/GameShould.swift
+++ ZombieSurvivorKataTests/GameShould.swift
@@ -10,9 +10,32 @@ class GameShould: XCTestCase {
         
         XCTAssertEqual(0, game.numberOfSurvivors)
     }
+    
+    func test_allowSurvivorsToBeAdded() {
+        
+        let gameWithNoSurvivors = Game()
+        
+        let gameWithOneSurvivor = gameWithNoSurvivors.addSurvivor(Survivor(name: "Papita"))
+        let gameWithTwoSurvivors = gameWithOneSurvivor.addSurvivor(Survivor(name: "Julian"))
+        
+        XCTAssertEqual(1, gameWithOneSurvivor.numberOfSurvivors)
+        XCTAssertEqual(2, gameWithTwoSurvivors.numberOfSurvivors)
+    }
 }
 
 struct Game {
     
-    let numberOfSurvivors = 0
+    let numberOfSurvivors: Int
+    
+    init() {
+        self.init(numberOfSurvivors: 0)
+    }
+    
+    private init(numberOfSurvivors: Int) {
+        self.numberOfSurvivors = numberOfSurvivors
+    }
+    
+    func addSurvivor(_ survivor: Survivor) -> Game {
+        return Game(numberOfSurvivors: numberOfSurvivors + 1)
+    }
 }
