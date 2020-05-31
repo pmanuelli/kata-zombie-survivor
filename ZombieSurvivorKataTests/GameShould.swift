0cdf9dffac2831484c368a4fb78263c2ee995fdb
Pablo Manuelli <pablo.manuelli@etermax.com>
2020-05-31 20:13:57 -0300

    Refactoring addSurvivor method

--- ZombieSurvivorKataTests/GameShould.swift
+++ ZombieSurvivorKataTests/GameShould.swift
@@ -49,13 +49,14 @@ struct Game {
     }
     
     func addSurvivor(_ survivor: Survivor) -> Game {
-        
-        var newSurvivors = survivors
-        
-        if !survivors.contains(where: { $0.name == survivor.name }) {
-            newSurvivors = newSurvivors + [survivor]
-        }
-        
-        return Game(survivors: newSurvivors)
+        return Game(survivors: createSurvivorsAdding(survivor))
+    }
+    
+    private func createSurvivorsAdding(_ survivor: Survivor) -> [Survivor] {
+        return containsSurvivorNamed(survivor.name) ? survivors : survivors + [survivor]
+    }
+    
+    private func containsSurvivorNamed(_ name: String) -> Bool {
+        return survivors.contains(where: { $0.name == name })
     }
 }
